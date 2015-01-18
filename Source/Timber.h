5f5559fde200419fca0989dccaaeab068fa41bba
Christian Noon <christian.noon@nike.com>
2015-01-18 12:07:39 -0800

    Added the base project to the repository...all empty implementation.

---
+++ Source/Timber.h
@@ -0,0 +1,12 @@
+//
+//  Timber.h
+//  Timber
+//
+//  Created by Christian Noon on 1/18/15.
+//  Copyright (c) 2015 Nike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double TimberVersionNumber;
+FOUNDATION_EXPORT const unsigned char TimberVersionString[];
