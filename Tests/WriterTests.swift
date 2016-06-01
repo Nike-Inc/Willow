2ca8cb1d01218e9712109ed7beb046420920f82f
Christian Noon <christian.noon@nike.com>
2016-06-01 09:14:25 -0700

    Added tests to fully cover the ConsoleWriter logic.

---
+++ Tests/WriterTests.swift
@@ -0,0 +1,59 @@
+//
+//  WriterTests.swift
+//
+//  Copyright (c) 2015-2016 Nike, Inc. (https://www.nike.com)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+import Willow
+import XCTest
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+import UIKit
+#elseif os(OSX)
+import Cocoa
+#endif
+
+class ConsoleWriterTestCase: XCTestCase {
+    func testThatConsoleWriterCanBeInitializedAndDeinitialized() {
+        // Given
+        let message = "Test Message"
+        let logLevel: LogLevel = .All
+        var writer: ConsoleWriter? = ConsoleWriter()
+
+        // When, Then
+        writer?.writeMessage(message, logLevel: logLevel, formatters: [])
+        writer = nil
+
+        // Then
+        XCTAssertNil(writer)
+    }
+
+    func testThatConsoleWriterCanWriteMessageToConsole() {
+        // Given
+        let message = "Test Message"
+        let logLevel: LogLevel = .All
+        let writer = ConsoleWriter()
+
+        // When, Then
+        writer.writeMessage(message, logLevel: logLevel, formatters: [TimestampFormatter()])
+    }
+}
