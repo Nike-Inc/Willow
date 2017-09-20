ec98f8c225edaae835e29f8c70809901232bc322
Christian Noon <christian.noon@nike.com>
2017-09-20 12:36:51 -0700

    Updated the copyright headers to use present instead of current year.

--- Example/Frameworks/WebServices/Request.swift
+++ Example/Frameworks/WebServices/Request.swift
@@ -1,7 +1,7 @@
 //
 //  Request.swift
 //
-//  Copyright (c) 2015-2017 Nike, Inc. (https://www.nike.com)
+//  Copyright (c) 2015-present Nike, Inc. (https://www.nike.com)
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
